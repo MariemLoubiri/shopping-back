2c912d138c07ecdec87831e24d23e44999fbae31
Mariem loubiri <mariemloubiri66@gmail.com>
2025-04-11 17:41:37 +0100

    msg3

--- server.js
+++ server.js
@@ -30,7 +30,12 @@ const PORT = process.env.PORT || 5000;
 
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*', // allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+  
 
 app.use(cookieParser());
 app.use(express.json());
